ae85d7c672bd88adcc4555bc7bc9d08ecf519bcc
lwl000 <1078607150@qq.com>
2021-05-03 16:10:10 +0800

    update

--- medmnist/models.py
+++ medmnist/models.py
@@ -10,21 +10,21 @@ class BasicBlock(nn.Module):
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-		self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-		self.downsample = downsample
+        self.downsample = downsample
         self.stride = stride
 
     def forward(self, x):
-		residual = x
-		out = self.conv1(x)
+        residual = x
+        out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-		if self.downsample is not None:
+        if self.downsample is not None:
             residual = self.downsample(x)
         out += residual
         out = self.relu(out)
@@ -44,12 +44,12 @@ class Bottleneck(nn.Module):
         self.conv3 = nn.Conv2d(planes, self.expansion *
                                planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-		self.relu = nn.ReLU(inplace=True)
-		self.downsample = downsample
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
         self.stride = stride
 
     def forward(self, x):
-		residual = x
+        residual = x
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -58,7 +58,7 @@ class Bottleneck(nn.Module):
         out = self.relu(out)
         out = self.conv3(out)
         out = self.bn3(out)
-		if self.downsample is not None:
+        if self.downsample is not None:
             residual = self.downsample(x)
         out += residual
         out = slef.relu(out)
@@ -72,15 +72,15 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7,
                                stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-		self.relu = nn.ReLU(inplace=True)
-		self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-		self.avgpool = nn.AvgPool2d(7)
+        self.avgpool = nn.AvgPool2d(7)
         self.linear = nn.Linear(512 * block.expansion, num_classes)
-		for m in self.modules():
+        for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
@@ -106,16 +106,16 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-		x = self.conv1(x)
-		x = self.bn1(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
         x = self.relu(x)
-		x = self.maxpool(x)
-		
+        x = self.maxpool(x)
+        
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-		
+        
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.linear(x)

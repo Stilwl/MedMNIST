8b8a86293bddd62b6e3e0e7f7c6955bac8fbe610
lwl000 <1078607150@qq.com>
2021-05-03 16:12:09 +0800

    update

--- medmnist/models.py
+++ medmnist/models.py
@@ -90,18 +90,18 @@ class ResNet(nn.Module):
 
     def _make_layer(self, block, planes, blocks, stride):
         downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
+        if stride != 1 or self.in_planes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
+                nn.Conv2d(self.in_planes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
+        layers.append(block(self.in_planes, planes, stride, downsample))
+        self.in_planes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+            layers.append(block(self.in_planes, planes))
 
         return nn.Sequential(*layers)
 

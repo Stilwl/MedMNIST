3c08f9a84e75315c14782de0b3d4df2f373ea5bc
lwl000 <1078607150@qq.com>
2021-05-03 17:11:47 +0800

    update

--- medmnist/models.py
+++ medmnist/models.py
@@ -79,6 +79,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.avgpool = nn.AvgPool2d(1, stride=1)
+        #self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.linear = nn.Linear(512 * block.expansion, num_classes)
         for m in self.modules():
             if isinstance(m, nn.Conv2d):

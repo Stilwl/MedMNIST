76643e75b49098c95646c5341003e31953f86103
lwl000 <1078607150@qq.com>
2021-05-19 21:24:03 +0800

    update

--- train.py
+++ train.py
@@ -2,11 +2,13 @@ import os
 import argparse
 from tqdm import trange
 import numpy as np
-import torch
+import torch,torchvision
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import subprocess
 
 from medmnist.models import ResNet18, ResNet50
 from medmnist.dataset import PathMNIST, ChestMNIST, DermaMNIST, OCTMNIST, PneumoniaMNIST, RetinaMNIST, \
@@ -15,7 +17,7 @@ from medmnist.evaluator import getAUC, getACC, save_results
 from medmnist.info import INFO
 
 
-def main(flag, netname, input_root, output_root, start_epoch, end_epoch, download, BS=128, LR=0.001):
+def main(args):
     ''' main function
     :param flag: name of subset
 
@@ -33,64 +35,74 @@ def main(flag, netname, input_root, output_root, start_epoch, end_epoch, downloa
         "organmnist_coronal": OrganMNISTCoronal,
         "organmnist_sagittal": OrganMNISTSagittal,
     }
-    DataClass = flag_to_class[flag]
+    args.data_name = args.data_name.lower()
+    args.net = args.net.lower()
+    DataClass = flag_to_class[args.data_name]
 
-    info = INFO[flag]
+    info = INFO[args.data_name]
     task = info['task']
     n_channels = info['n_channels']
     n_classes = len(info['label'])
 
     # start_epoch = 0
-    lr = LR
-    batch_size = BS
+    lr = args.lr
+    batch_size = args.bs
     # val_auc_list = []
-    dir_path = os.path.join(output_root, '%s_checkpoints' % (flag))
+    dir_path = os.path.join(args.output_root, '%s_checkpoints' % (args.data_name))
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
-    filename = os.path.join(dir_path, 'save_data.txt')
-    if not os.path.isfile(filename):
-        os.system(r"touch {}".format(filename))#调用系统命令行来创建文件
+    data_path = os.path.join(dir_path, 'save_data.txt')
+    if not os.path.isfile(data_path):
+        os.system(r"touch {}".format(data_path))#调用系统命令行来创建文件
 
     print('==> Preparing data...')
     train_transform = transforms.Compose(
-        [transforms.ToTensor(),
+        [transforms.Resize([32,32]),
+         transforms.ToTensor(),
          transforms.Normalize(mean=[.5], std=[.5])])
 
     val_transform = transforms.Compose(
-        [transforms.ToTensor(),
+        [transforms.Resize([32,32]),
+         transforms.ToTensor(),
          transforms.Normalize(mean=[.5], std=[.5])])
 
     test_transform = transforms.Compose(
-        [transforms.ToTensor(),
+        [transforms.Resize([32,32]),
+         transforms.ToTensor(),
          transforms.Normalize(mean=[.5], std=[.5])])
 
-    train_dataset = DataClass(root=input_root,
+    train_dataset = DataClass(root=args.input_root,
                                     split='train',
                                     transform=train_transform,
-                                    download=download)
+                                    download=args.download)
     train_loader = data.DataLoader(dataset=train_dataset,
                                    batch_size=batch_size,
                                    shuffle=True)
-    val_dataset = DataClass(root=input_root,
+    val_dataset = DataClass(root=args.input_root,
                                   split='val',
                                   transform=val_transform,
-                                  download=download)
+                                  download=args.download)
     val_loader = data.DataLoader(dataset=val_dataset,
                                  batch_size=batch_size,
                                  shuffle=True)
-    test_dataset = DataClass(root=input_root,
+    test_dataset = DataClass(root=args.input_root,
                                    split='test',
                                    transform=test_transform,
-                                   download=download)
+                                   download=args.download)
     test_loader = data.DataLoader(dataset=test_dataset,
                                   batch_size=batch_size,
                                   shuffle=True)
 
+    # 可视化四张图片
+    if args.verbose:
+        datashow(train_loader)
+
     print('==> Building and training model...')
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    if netname == 'resnet50':
+
+    if args.net=='resnet50':
         model = ResNet50(in_channels=n_channels, num_classes=n_classes).to(device)
     else:
         model = ResNet18(in_channels=n_channels, num_classes=n_classes).to(device)
@@ -100,11 +112,14 @@ def main(flag, netname, input_root, output_root, start_epoch, end_epoch, downloa
     else:
         criterion = nn.CrossEntropyLoss()
 
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    if args.optim=='SGD':
+        optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    else:
+        optimizer = optim.Adam(model.parameters(), lr=lr)
 
-    for epoch in trange(start_epoch, end_epoch):
+    for epoch in trange(args.start_epoch, args.end_epoch):
         if epoch>=1:
-            save_data = np.loadtxt(os.path.join(dir_path, 'save_data.txt'))
+            save_data = np.loadtxt(data_path)
             save_data = np.reshape(save_data,(-1,3))
             val_auc_list = save_data[:, 1]
             model_path = os.path.join(dir_path, 'ckpt_%d_auc_%.5f.pth' % (epoch-1, val_auc_list[epoch-1]))
@@ -112,7 +127,7 @@ def main(flag, netname, input_root, output_root, start_epoch, end_epoch, downloa
         train(model, optimizer, criterion, train_loader, device, task)
         val(model, val_loader, device, task, dir_path, epoch)
 
-    save_data = np.loadtxt(os.path.join(dir_path, 'save_data.txt'))
+    save_data = np.loadtxt(data_path)
     auc_list = save_data[:, 1]
     index = auc_list.argmax()
     print('epoch %s is the best model' % (index))
@@ -121,22 +136,22 @@ def main(flag, netname, input_root, output_root, start_epoch, end_epoch, downloa
     restore_model_path = os.path.join(
         dir_path, 'ckpt_%d_auc_%.5f.pth' % (index, auc_list[index]))
     model.load_state_dict(torch.load(restore_model_path)['net'])
-    test(model,
-         'train',
-         train_loader,
-         device,
-         flag,
-         task,
-         output_root=output_root)
-    test(model, 'val', val_loader, device, flag, task, output_root=output_root)
-    test(model,
-         'test',
-         test_loader,
-         device,
-         flag,
-         task,
-         output_root=output_root)
+    test(model, 'train', train_loader, device, task, args)
+    test(model, 'val', val_loader, device, task, args)
+    test(model, 'test', test_loader, device, task, args)
 
+# 查看数据（可视化数据）
+def datashow(train_loader):
+    # 可视化样本，下面是输出了训练集中前4个样本
+    fig, ax = plt.subplots(nrows=2, ncols=2, sharex='all', sharey='all')
+    ax = ax.flatten()
+    images, label = next(iter(train_loader))
+    for i in range(4):
+        img = images[i].reshape(32, 32)
+        # ax[i].imshow(img,cmap='Greys')
+        ax[i].imshow(img)
+    plt.tight_layout()
+    plt.show()
 
 def train(model, optimizer, criterion, train_loader, device, task):
     ''' training function
@@ -216,13 +231,12 @@ def val(model, val_loader, device, task, dir_path, epoch):
     data_file.close()
 
 
-def test(model, split, data_loader, device, flag, task, output_root=None):
+def test(model, split, data_loader, device, task, args):
     ''' testing function
     :param model: the model to test
     :param split: the data to test, 'train/val/test'
     :param data_loader: DataLoader of data
     :param device: cpu or cuda
-    :param flag: subset name
     :param task: task of current dataset, binary-class/multi-class/multi-label, binary-class
 
     '''
@@ -254,8 +268,8 @@ def test(model, split, data_loader, device, flag, task, output_root=None):
         acc = getACC(y_true, y_score, task)
         print('%s AUC: %.5f ACC: %.5f' % (split, auc, acc))
 
-        if output_root is not None:
-            output_dir = os.path.join(output_root, flag)
+        if args.output_root is not None:
+            output_dir = os.path.join(args.output_root, args.data_name)
             if not os.path.exists(output_dir):
                 os.mkdir(output_dir)
             output_path = os.path.join(output_dir, '%s.csv' % (split))
@@ -301,29 +315,18 @@ if __name__ == '__main__':
                         type=int, 
                         default=128,
                         help='batch size')
-    parser.add_argument('--netname',
+    parser.add_argument('--net',
                         type=str, 
-                        default='ResNet18',
-                        help='kind of resnet')
+                        default='resnet18',
+                        help='ResNet name')
+    parser.add_argument('--optim',
+                        default='Adam',
+                        help='The optimizer',
+                        type=str)
+    parser.add_argument('--verbose',
+                        default=False,
+                        help='Show part of the dataset',
+                        type=bool)
 
     args = parser.parse_args()
-    data_name = args.data_name.lower()
-    input_root = args.input_root
-    output_root = args.output_root
-    # end_epoch = args.num_epoch
-    start_epoch = args.start_epoch
-    end_epoch = args.end_epoch
-    download = args.download
-    batch_size = args.bs
-    learning_rate = args.lr
-    netname = args.netname.lower()
-    main(data_name,
-         netname,
-         input_root,
-         output_root,
-         start_epoch=start_epoch,
-         end_epoch=end_epoch,
-         download=download,
-         BS=batch_size,
-         LR=learning_rate,
-         )
+    main(args)
